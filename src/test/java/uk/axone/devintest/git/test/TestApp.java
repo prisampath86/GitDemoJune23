596724ae4808250fdf7dacb52983c88ebf61ff29
prisampath86 <priya.sampath86@gmail.com>
2023-06-07 20:05:45 +0100

    Jira 1234 - First Commit

---
+++ src/test/java/uk/axone/devintest/git/test/TestApp.java
@@ -0,0 +1,13 @@
+package uk.axone.devintest.git.test;
+
+import org.testng.annotations.Test;
+
+public class TestApp
+    {
+        @Test
+        public void test1()
+        {
+            System.out.println("Running Test 1");
+        }
+
+    }
